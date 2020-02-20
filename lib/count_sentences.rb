a43bf86e33cb86dbf26d38ecb94450b0ac0f2416
nickzaf95 <nicholaszafiriou@gmail.com>
2020-02-20 09:47:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    period = self.split(/[.?!]/)
+    i = 0
+    while i < period.size do
+      if period[i] == ""
+        period.delete_at(i)
+      else
+        i += 1
+      end
+    end
+    return period.size
   end
 end
